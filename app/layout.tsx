e393b711785205d3586b1e83f7656a9e4c26fe84
Eduardo Lima <159482135+educlima@users.noreply.github.com>
2025-06-23 20:02:35 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Versao 0 App',
+  description: 'Versao 0 do projeto',
+ 
 }
 
 export default function RootLayout({
